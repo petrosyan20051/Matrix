b01d038999b343a39c4d8545d17459d0eb1eb4e5
Максим Ледовский <santech_montage@mail.ru>
2023-10-19 07:38:23 +0300

    Добавьте файлы проекта.

---
+++ Matrix/Matrix.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int i = 0, j = 0, max; //i - число строк матрицы, j - число столбцов
+
+	cout << "Ведите количества строк i и столбцов j (i > 0, j > 0): ";
+	while (i <= 0 || j << 0)
+	{
+		cin >> i >> j;
+		if (i <= 0 || j <= 0)
+			cout << "Введены неверные значения, попробуйте ещё раз!";
+	}
+	
+	int matrix[i][j]; // вводим матриц
+}
