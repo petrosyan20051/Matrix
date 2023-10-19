83530568e7e4dbfe436ce43cce54bcb6e5c5fde7
Максим Ледовский <santech_montage@mail.ru>
2023-10-19 08:30:08 +0300

    -

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -12,13 +12,37 @@ int main()
 
 	int i = 0, j = 0, max; //i - число строк матрицы, j - число столбцов
 
-	cout << "Ведите количества строк i и столбцов j (i > 0, j > 0): ";
-	while (i <= 0 || j << 0)
+	cout << "Введите количества строк i и столбцов j (i > 0, j > 0): ";
+	while (i <= 0 || j <= 0)
 	{
 		cin >> i >> j;
 		if (i <= 0 || j <= 0)
-			cout << "Введены неверные значения, попробуйте ещё раз!";
+			cout << "Введены неверные значения, попробуйте ещё раз!" << endl;
 	}
-	
-	int matrix[i][j]; // вводим матриц
+	int** matrix{ new int* [i] }; // создаём динамический массив с строками
+	for (int temp = 0; temp < j; temp++) // создаём динамический массив со столбцами
+		matrix[temp] = new int[j];
+
+	cout << "Введите элементы матрицы построчно" << endl; // заполняем матрицу
+	for (int temp_row = 0; temp_row < i; temp_row++)
+	{
+		for (int temp_column = 0; temp_column < j; temp_column++)
+		{
+			cout << "matrix[" << temp_row << "][" << temp_column << "] = ";
+			cin >> matrix[temp_row][temp_column];
+		}
+	}
+	/*for (int temp_row = 0; temp_row < i; temp_row++) // выводим матрицу в консоль
+	{
+		for (int temp_column = 0; temp_column < j; temp_column++)
+		{
+			cout << matrix[temp_row][temp_column] << " ";
+			if (temp_column == j - 1)
+				cout << endl;
+		}
+	}
+	*/
+
+
+	return 0;
 }
