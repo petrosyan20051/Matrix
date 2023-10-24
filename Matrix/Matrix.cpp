34f59eaacd2200f39385425772640d7bc58b6287
Максим Ледовский <santech_montage@mail.ru>
2023-10-24 19:51:05 +0300

    -

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -10,18 +10,18 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int i = 0, j = 0; //i - число строк матрицы, j - число столбцов
+	int i = 1, j = 1; //i - число строк матрицы, j - число столбцов
 
-	cout << "Введите количества строк i и столбцов j (i > 0, j > 0): ";
-	while (i <= 0 || j <= 0)
+	cout << "Введите количества строк i и столбцов j (i > 1, j > 1): ";
+	while (i <= 1 || j <= 1)
 	{
 		cin >> i >> j;
-		if (i <= 0 || j <= 0)
+		if (i <= 1 || j <= 1)
 			cout << "Введены неверные значения, попробуйте ещё раз!" << endl;
 	}
-	int** matrix{ new int* [i] }; // создаём динамический массив с строками
+	float** matrix{ new float* [i] }; // создаём динамический массив с строками
 	for (int temp = 0; temp < j; temp++) // создаём динамический массив со столбцами
-		matrix[temp] = new int[j];
+		matrix[temp] = new float[j];
 
 	cout << "Введите элементы матрицы построчно" << endl; // заполняем матрицу
 	for (int temp_row = 0; temp_row < i; temp_row++)
@@ -45,20 +45,20 @@ int main()
 	cout << endl;
 	*/
 
-	int s = 1, n; // s - предполагаемая нулевая сумма элементов какого-то столбца, n - номер "0" столбца
+	int s = 0, n = -1; // s - предполагаемая нулевая сумма элементов какого-то столбца, n - номер "0" столбца
 	for (int temp_column = 0; temp_column < j; temp_column++)
 	{
+		s = 0;
 		for (int temp_row = 0; temp_row < i; temp_row++)
 		{
 			s += matrix[temp_row][temp_column];
 		}
-		if (s - 1 == 0)
+		if (s == 0)
 		{
-			//cout << temp_column << " столбец";
+			//cout << temp_column << " столбец" << endl;
 			n = temp_column; // запоминаем номер "0" столбца
 			break;
 		}
-
 	}
 
 	int max = matrix[1][0]; // максимальное значение в чётных строках (начальное значение берём первое)
